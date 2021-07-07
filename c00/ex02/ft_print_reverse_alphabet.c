393aa2db200210415b07241b320c7d4096db0720
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-07 23:15:26 +0200

    cimmot

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 23:13:49 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/07 23:14:36 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
