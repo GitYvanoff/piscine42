0a75886832aac5bd23b021191d4332ad2d5da02f
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-08 16:42:53 +0200

    Apres 4h de reflexion intense..

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/08 13:02:59 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/08 16:42:06 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_comb_2(void)
+{
+	char	a;
+	char	b;
+	
+	a = 0;
+	b = 1;
+	while (a < 99)
+	{
+		ft_putchar(a / 10 + '0');
+		ft_putchar(a % 10 + '0');
+		ft_putchar(' ');
+		ft_putchar(b / 10 + '0');
+		ft_putchar(b % 10 + '0');
+		if (a < 98)
+		{
+			write(1, ", ", 2);
+		}
+		if (++b > 99)
+		{
+			b = ++a + 1;
+		}
+	}
+}
+
+int	main(void)
+{
+	print_comb_2();
+}
