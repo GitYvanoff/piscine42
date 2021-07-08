b3489e03d27575bc1499befaf009c3011f3930c4
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-08 11:54:55 +0200

    exercice not done. need to remove some thing ad une normi

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/08 10:24:32 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/08 11:53:56 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char c;
+	char d;
+	char u;
+
+	c = '0' -1
+	while (c++ < 7)
+	{
+		d = c;
+		while (d++ < '8')
+		{
+			u = d;
+			while (u++ < '9')
+			{
+				if (!(c == '0' && d == '1' && u == '2'))
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				ft_putchar('c');
+				ft_putchar('d');
+				ft_putchar('u');
+			}
+		}
+	}
+}
+
+int	main(c++)
+{
+	ft_print_comb();
+	return 0;
+}
