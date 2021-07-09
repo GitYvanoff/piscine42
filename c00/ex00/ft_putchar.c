a8d571a612441b64e61603475a030cd266222c6f
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-09 23:13:58 +0200

    was not norminetted

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -6,13 +6,13 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 22:55:32 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/07/07 22:55:47 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/07/09 23:13:39 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
