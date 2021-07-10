d0213ef42da1527bda95af516e625f261c7b0cc0
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-10 23:34:34 +0200

    norminetted

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -6,25 +6,24 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 19:31:26 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/07/10 21:43:37 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/07/10 23:33:46 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	int	count;
 
 	count = 0;
-	while (*(str + count) != '\0') // null terminated string
+	while (*(str + count) != '\0')
 	{
 		count++;
 	}
 	write(1, str, count);
 }
-
-int	main(void)
-{
-	ft_putstr("hello");
-}
+//int	main(void)
+//{
+//	ft_putstr("hello");
+//}
