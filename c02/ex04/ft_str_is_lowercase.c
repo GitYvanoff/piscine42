329f57d89a951e56686089cab67d3c58a2dc2c58
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-12 12:21:47 +0200

    norminetted and commented main and include section for test purpose

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 12:15:43 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/12 12:21:16 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_str_is_lowercase(char	*str)
+{
+	int	count;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		if (!(*(str + count) <= 'z' && *(str + count) >= 'a'))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/*int main(void)
+{
+	char	tab_test[] = "aefsdfGds";
+
+	printf("%i", ft_str_is_lowercase(tab_test));
+}*/
