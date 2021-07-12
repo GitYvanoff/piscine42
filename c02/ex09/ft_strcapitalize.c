6535ce0f9ad8b2f63a6fb850de6bf54491285472
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-12 18:24:22 +0200

    file norminetted and commented the main and include for test purposes

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 14:11:26 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/12 18:18:20 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int		ft_islower(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+int		ft_isupper(char c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+char	*ft_strcapitalize(char	*str)
+{
+	int		i;
+	int		set_upper;
+
+	i = 0;
+	set_upper = 1;
+	while (str[i])
+	{
+		if (ft_islower(str[i]) || ft_isupper(str[i]) || ft_isdigit(str[i]))
+		{
+			if (set_upper && ft_islower(str[i]))
+				str[i] = str[i] - 32;
+			else if (!set_upper && ft_isupper(str[i]))
+				str[i] = str[i] + 32;
+			set_upper = 0;
+		}
+		else
+			set_upper = 1;
+		i++;
+	}
+	return (str);
+}
+int	main(void)
+{
+	char tab_test[] = "saLut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+	printf("%s", ft_strcapitalize(tab_test));
+}
