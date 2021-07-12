7144ef6252df991a6d3fffa80f1b97d4a444c83d
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-12 13:55:13 +0200

    norminetted and commented include and main for test purposes

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 13:51:55 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/12 13:54:45 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		if (*(str + count) <= 'Z' && *(str + count) >= 'A')
+		{
+			*(str + count) = *(str + count) + ' ';
+		}
+		count++;
+	}
+	return (str);
+}
+
+/*int main(void)
+{
+	char	tab_test[] = "S_DaakueoF";
+	
+	printf("%s", tab_test);
+	printf("%s", ft_strlowcase(tab_test));
+}*/
