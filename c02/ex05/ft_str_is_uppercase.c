f7abd3575cb5f4040dda3b25bb135d81289dd15e
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-12 12:27:06 +0200

    normionetted and commented main and include for test purpose

---
+++ c02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 12:23:00 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/12 12:26:44 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		if (!(*(str + count) <= 'Z' && *(str + count) >= 'A'))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/*int main(void)
+{
+	char	tab_test[] = "SDFS_DF";
+
+	printf("%i", ft_str_is_uppercase(tab_test));
+}*/
