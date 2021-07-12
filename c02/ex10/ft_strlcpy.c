cf422c9a369bb022736aa569df86a1f31fd647cd
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-12 21:21:49 +0200

    norminetted and commented main and include for test purpose

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/12 20:33:45 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/12 21:21:17 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	count;
+	unsigned int	index;
+
+	count = 0;
+	index = 0;
+	while (src[count] != '\0')
+		count++;
+	if (size != 0)
+	{
+		while (src[index] != '\0' && index < (size - 1))
+		{
+			dest[index] = src[index];
+			index++;
+		}
+		dest[index] = '\0';
+	}
+	return (count);
+}
+
+/*int	main(void)
+{
+	char tab_src[6] = "salut";
+	char tab_dest[6];
+
+	ft_strlcpy(tab_dest, tab_src, 7);
+	printf("%s", tab_dest);
+}*/
