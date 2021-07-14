e2c9cffbffcd5ef16ce118049e8a5c036da49f50
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-14 17:14:11 +0200

    ok whz not

---
+++ c04/ex01/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/14 12:58:34 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/14 17:13:49 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char	*str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	write(1, str, count);
+}
+
+/*int	main(void)
+{
+	char	*ptr = "ceci est un test !";
+
+	ft_putstr(&ptr[4]);
+}*/
