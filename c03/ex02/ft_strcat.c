0c82195aa581a3a47d84b534c391c5127007f428
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-15 18:48:41 +0200

    test this beautifull fucking code

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -6,12 +6,12 @@
 /*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 11:27:01 by ypetruzz          #+#    #+#             */
-/*   Updated: 2021/07/13 13:02:26 by ypetruzz         ###   ########.fr       */
+/*   Updated: 2021/07/15 18:47:37 by ypetruzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string.h>
-//#include <stdio.h>
+// #include <string.h>
+// #include <stdio.h>
 
 char	*ft_strcat(char	*dest, char	*src)
 {
@@ -38,30 +38,30 @@ char	*ft_strcat(char	*dest, char	*src)
 	return (dest);
 }
 
-/*int	main(void)
-{
-	char	src[50];
-	char	dest[50];
-	char	src2[50];
-	char	dest2[50];
-
-	strcpy(src,  "This is source");
-	strcpy(dest, "This is destination");
-	strcpy(src2,  "This is source");
-	strcpy(dest2, "This is destination");
-	
-	printf("TEST ORINIGAL STRCAT\n");
-	printf("%s\n", src);
-	printf("%s\n", dest);
-	strcat(dest, src);
-	printf("TRAITEMENT PAR STRCAT :\n");
-	printf("%s\n\n", dest);
-
-	printf("TEST ft_strcat ft_strcat\n");
-	printf("%s\n", src2);
-	printf("%s\n", dest2);
-	ft_strcat(dest2, src2);
-	printf("TRAITEMENT PAR ft_strcat :\n");
-	printf("%s\n", src2);
-	printf("%s\n", dest2);
-}*/
+// int	main(void)
+// {
+// 	char	src[50];
+// 	char	dest[32];
+// 	char	src2[50];
+// 	char	dest2[32];
+// 
+// 	strcpy(src,  "This is source");
+// 	strcpy(dest, "This is destination");
+// 	strcpy(src2,  "This is source");
+// 	strcpy(dest2, "This is destination");
+// 	
+// 	printf("TEST ORINIGAL STRCAT\n");
+// 	printf("%s\n", src);
+// 	printf("%s\n", dest);
+// 	//strcat(dest, src);
+// 	printf("TRAITEMENT PAR STRCAT :\n");
+// 	printf("%s\n\n", dest);
+// 
+// 	printf("TEST ft_strcat ft_strcat\n");
+// 	printf("%s\n", src2);
+// 	printf("%s\n", dest2);
+// 	ft_strcat(dest2, src2);
+// 	printf("TRAITEMENT PAR ft_strcat :\n");
+// 	printf("%s\n", src2);
+// 	printf("%s\n", dest2);
+// }
