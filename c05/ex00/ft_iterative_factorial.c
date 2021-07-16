34ffe212424dd0f63bcf921b0080f12f40633abe
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-16 21:43:20 +0200

    sdf

---
+++ c05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/16 21:06:06 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/16 21:42:50 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+
+int	ft_iterative_factorial(int	nb)
+{
+	int	n;
+
+	if (nb < 1)
+		return (!nb);
+	n = 1;
+	while (nb)
+	{
+		n = n * nb;
+		nb--;
+	}
+	return (n);
+}
+
+/*int	main(int	argc, char	**argv)
+{
+	int	res;
+	
+	if(argc > 2)
+		return (1);
+
+	res = atoi(argv[1]);
+	printf("%i\n", ft_iterative_factorial(res));
+}*/
