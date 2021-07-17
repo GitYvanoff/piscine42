1f863c1749222703cd0215d13e6385bf12654a4e
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-17 02:31:08 +0200

    everzthing is commented and test sequence works fine

---
+++ c05/ex02/ft_iterative_power.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/16 22:11:51 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/17 02:30:45 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+
+int	ft_iterative_power(int	nb, int	power)
+{
+	int	count;
+	int	res;
+	
+	count = 0;
+	res = 1;
+	while (count < power)
+	{
+		res = res * nb;
+		count++;
+	}
+	return (res);
+}
+
+/*int	main(int	argc, char	**argv)
+{
+	int	value_1;
+	int	value_2;
+	int	res;
+
+	res = 0;
+	value_1 = 0;
+	value_2 = 0;
+	if (argc > 3)
+		return (1);
+	value_1 = atoi(argv[1]);
+	value_2 = atoi(argv[2]);
+	res = ft_iterative_power(value_1, value_2);
+	printf("%i", res);
+}*/
