6cf0f34affcb1e3aea4ba52685e92e3236faf754
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-18 13:51:01 +0200

    this one is ez lol

---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/18 13:48:20 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/18 13:50:50 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		count++;
+	}
+	write(1, str, count);
+}
+
+int	main(int	argc, char	**argv)
+{
+	if (argc != 1)
+		return 1;
+	ft_putstr(argv[0]);
+}
