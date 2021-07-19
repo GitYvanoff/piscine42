e1ea7dfef467b4dfbd3626bc2786622346872808
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-19 15:27:26 +0200

    Premier jet fait avec Gab

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 00:22:02 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/19 14:13:28 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*(str + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_strdup(char	*src)
+{
+	int		size;
+	char	*str;
+	char	*res;
+
+	size = ft_strlen(src);
+	str =  malloc(sizeof (char) * size);
+	res = str;
+	while (*src != '\0')
+	{
+		*str = *src;
+		++str;
+		++src;
+	}
+	*str = '\0';
+	return (res);
+}
+
+int	main(int	argc, char	**argv)
+{
+	char	*ptr;
+
+	if (argc != 2)
+		return (1);
+	ptr = ft_strdup(argv[1]);
+	printf("%s\n", ptr);
+}
