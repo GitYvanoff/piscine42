fd837e0184794a4f58806c1571573dc74a49ffcf
Yvan Petruzzi <ypetruzz@student.42lausanne.ch>
2021-07-21 21:23:21 +0200

    really ??

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ypetruzz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/21 21:04:50 by ypetruzz          #+#    #+#             */
+/*   Updated: 2021/07/21 21:21:17 by ypetruzz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#infdef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef	char	t_bool;
+# define FALSE 0
+# define TRUE 1
+
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+# define EVEN(X) ((X % 2) ? 0 : 1)
+
+#endif
