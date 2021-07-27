7a9a4a8211a44c2210bcaeaa3bed2d9fc9283f9d
Mattia Torsello <torsello.mattia@gmail.com>
2021-07-27 21:07:41 +0200

    loader working

--- ex00/ft_map_reader.c
+++ ex00/ft_map_reader.c
@@ -20,17 +20,53 @@ char	*get_first_line(char	*map)
 	return(first_line);
 }
 
-int	ft_map_reader(int ac, char **av)
+char    *go_to_char(char *str, char c)
 {
-	char *map_tmp;
+    while (*str && *str != c)
+        str++;
+    return (str);
+}
+
+char    **map_loader(char *str, int lines_count)
+{
+    char    **map;
+    int     y;
+
+    map = (char **)malloc(sizeof(char *) * lines_count);
+    str = go_to_char(str, '\n');
+    str++;
+    y = 0;
+    while (y < lines_count)
+    {
+        map[y] = ft_strdup_stop(str, '\n');
+        y++;
+        str = go_to_char(str, '\n');
+        str++;
+    }
+    return (map);
+}
+
+void    map_validator(char **map)
+{
+
+    return ;
+}
+
+int     ft_map_reader(int ac, char **av)
+{
+	char *map_tmp; //code smell
 	char *first_line;
-	
+    char **map;
+
+
 	if (ac >= 1)
 	{
-		map_tmp = ft_read_console();
+		map_tmp = ft_read_console(); //eurk
 		printf("je suis la map\n %s", map_tmp);
 		first_line = get_first_line(map_tmp);
 		printf("\n Je suis la premiere ligne : %s \n", first_line);
+        map = map_loader(map_tmp, 10);
+        map_validator(map, 10);
 	}
 	else
 	{
